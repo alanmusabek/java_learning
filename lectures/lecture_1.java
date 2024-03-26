660965f8d43e498e19ab3a024197a98e4b827548
Alan M. <waalanmusa@gmail.com>
2024-03-26 14:39:20 +0600

    lecture_1

---
+++ lectures/lecture_1.java
@@ -0,0 +1,13 @@
+// variables
+public class Variables {
+    public static void main(String[] args) {
+        // int is the whole number, int is primitive type, int don't store fractions or decimals
+        // int is the keyword we write before the name of the variable
+        int age = 30;
+        System.out.println(age);
+
+        // 
+        String name = "John";
+        System.out.println(name);
+    }
+}
