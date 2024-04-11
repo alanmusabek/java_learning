fea2e33d5596cf7ffdff255791c95101103130e6
Alan M. <waalanmusa@gmail.com>
2024-04-11 12:23:55 +0600

    Organize folders and add lecture project

---
+++ projects/simple_car_payment_calculator.java
@@ -0,0 +1,24 @@
+// The program for calculating car loan payment.
+public class CarLoan {
+	public static void main(String[] args) {
+    // initial data
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+    // check if a car loan is valid.
+    if(loanLength <= 0 || interestRate <= 0) {
+      System.out.println("Error! You must take out a valid car loan.");
+    } else if(downPayment >= carLoan) { // check if user's balance is more than the car loan;
+      System.out.println("The car can be paid in full. :)");
+    } else {
+      // calculate the monthly payment
+      int remainingBalance = carLoan - downPayment;
+      int months = loanLength * 12;
+      int monthlyBalance = remainingBalance / months;
+      int interest = (monthlyBalance * interestRate) / 100;
+      int monthlyPayment = monthlyBalance + interest;
+      System.out.println(monthlyPayment);
+    }
+	}
+}
