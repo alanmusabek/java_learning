8ae43031313f7bbec5964e5fcf2a5b07b10d960a
Alan M. <waalanmusa@gmail.com>
2024-04-12 22:47:37 +0600

    Add a basic calculator

---
+++ projects/Basic calculator/Calculator.java
@@ -0,0 +1,30 @@
+// A Basic Calculator
+// A project that uses classes, methods, and objects to create a simple arithmetic calculator
+public class Calculator {
+    public Calculator() {
+
+    }
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    public int modulo(int a, int b) {
+        return a % b;
+    }
+
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+        System.out.println(myCalculator.add(5, 7));
+
+        System.out.println(myCalculator.subtract(45, 11));
+        
+    }
+}
